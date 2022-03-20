94d295f178308431562ac04ec087a55beb57c39e
Rholang <rholang@protonmail.com>
2022-03-20 22:38:38 +0100

    Updates

--- assets/_name_.d07f438a.js
+++
@@ -1,2 +0,0 @@
-import{f as l,i,b as o,L as a,t as s}from"./vendor.8b387e40.js";const u=s("<div><div><strong>/hi</strong> route, built at: 2022-03-20T21:27:22.665Z</div><p>Hi: </p><br></div>");function c(){const n=l();return(()=>{const t=u.cloneNode(!0),r=t.firstChild,e=r.nextSibling;return e.firstChild,e.nextSibling,i(e,()=>n.name,null),i(t,o(a,{href:"/",children:"Go Home"}),null),t})()}export{c as default};
-//# sourceMappingURL=_name_.d07f438a.js.map
---
+++ assets/about.5dd3159b.js
@@ -0,0 +1,2 @@
+import{i as e,b as n,L as o,t as r}from"./vendor.8b387e40.js";const i=r("<div><div><strong>/about</strong> route, built at: 2022-03-20T21:38:31.768Z</div><br></div>");function a(){return(()=>{const t=i.cloneNode(!0);return t.firstChild.nextSibling,e(t,n(o,{href:"/",children:"Go Home"}),null),t})()}export{a as default};
+//# sourceMappingURL=about.5dd3159b.js.map
