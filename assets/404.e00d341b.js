a9ad289d74e7cc4d334abd1775094915bddee50f
Rholang <rholang@protonmail.com>
2022-03-20 22:54:31 +0100

    Updates

---
+++ assets/404.e00d341b.js
@@ -0,0 +1,2 @@
+import{t}from"./vendor.93ea2724.js";const o=t("<section><h1>404: Not Found</h1><p>It&apos;s gone \u{1F61E}</p></section>");function n(){return o.cloneNode(!0)}export{n as default};
+//# sourceMappingURL=404.e00d341b.js.map
--- assets/about.62e5baaa.js
+++
@@ -1,2 +0,0 @@
-import{i as e,b as n,L as o,t as r}from"./vendor.8b387e40.js";const i=r("<div><div><strong>/about</strong> route, built at: 2022-03-20T21:40:50.124Z</div><br></div>");function a(){return(()=>{const t=i.cloneNode(!0);return t.firstChild.nextSibling,e(t,n(o,{href:"/",children:"Go Home"}),null),t})()}export{a as default};
-//# sourceMappingURL=about.62e5baaa.js.map
