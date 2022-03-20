ecfdbe0830061d09207568e61839cc028f0eeec3
Rholang <rholang@protonmail.com>
2022-03-20 22:09:47 +0100

    Updates

--- assets/_name_.855a4cb8.js
+++
@@ -1,2 +0,0 @@
-import{f as l,i,b as o,L as a,t as s}from"./vendor.8b387e40.js";const u=s("<div><div><strong>/hi</strong> route, built at: 2022-03-20T20:10:20.607Z</div><p>Hi: </p><br></div>");function c(){const n=l();return(()=>{const t=u.cloneNode(!0),r=t.firstChild,e=r.nextSibling;return e.firstChild,e.nextSibling,i(e,()=>n.name,null),i(t,o(a,{href:"/",children:"Go Home"}),null),t})()}export{c as default};
-//# sourceMappingURL=_name_.855a4cb8.js.map
