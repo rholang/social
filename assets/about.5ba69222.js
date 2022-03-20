4b2210a70c20b3c5853eb086bb551e9d2cf3f525
Rholang <rholang@protonmail.com>
2022-03-20 20:13:53 +0100

    Updates

---
+++ assets/about.5ba69222.js
@@ -0,0 +1,2 @@
+import{i as e,b as n,L as o,t as r}from"./vendor.8b387e40.js";const i=r("<div><div><strong>/about</strong> route, built at: 2022-03-20T19:13:47.599Z</div><br></div>");function a(){return(()=>{const t=i.cloneNode(!0);return t.firstChild.nextSibling,e(t,n(o,{href:"/",children:"Go Home"}),null),t})()}export{a as default};
+//# sourceMappingURL=about.5ba69222.js.map
