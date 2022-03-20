daa14298ccf5307ade6d3aaf1aa4e244061261f0
Rholang <rholang@protonmail.com>
2022-03-20 22:40:57 +0100

    Updates

---
+++ assets/_name_.aca79d31.js
@@ -0,0 +1,2 @@
+import{f as l,i,b as o,L as a,t as s}from"./vendor.8b387e40.js";const u=s("<div><div><strong>/hi</strong> route, built at: 2022-03-20T21:40:50.124Z</div><p>Hi: </p><br></div>");function c(){const n=l();return(()=>{const t=u.cloneNode(!0),r=t.firstChild,e=r.nextSibling;return e.firstChild,e.nextSibling,i(e,()=>n.name,null),i(t,o(a,{href:"/",children:"Go Home"}),null),t})()}export{c as default};
+//# sourceMappingURL=_name_.aca79d31.js.map
